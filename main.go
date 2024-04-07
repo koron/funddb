ba9bf838ed31c985df602ac3957896a4a4ca4f7d
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-04-07 21:54:31 +0900

    drop rev1

--- main.go
+++ main.go
@@ -1,169 +1,23 @@
-//go:build rev1
-
 package main
 
 import (
-	"bufio"
-	"context"
-	"database/sql"
-	"errors"
-	"flag"
-	"fmt"
 	"log"
 	"os"
-	"strings"
-	"time"
 
-	"github.com/koron/funddb/internal/ammufg"
-	"github.com/koron/funddb/internal/fidelity"
+	"github.com/koron-go/subcmd"
+	"github.com/koron/funddb/subcmds/database"
+	"github.com/koron/funddb/subcmds/fund"
+	"github.com/koron/funddb/subcmds/price"
 )
 
-type FundData struct {
-	Scheme string
-	ID     string
-	Date   time.Time
-	Price  int64
-}
-
-type FundDataSource interface {
-	Scheme() string
-	ID() string
-	Date() time.Time
-	Price() int64
-}
-
-func fill(src FundDataSource) FundData {
-	return FundData{
-		Scheme: src.Scheme(),
-		ID:     src.ID(),
-		Date:   src.Date(),
-		Price:  src.Price(),
-	}
-}
-
-func get(ctx context.Context, name string) (FundDataSource, error) {
-	parts := strings.SplitN(name, ":", 2)
-	if len(parts) < 2 {
-		return nil, fmt.Errorf("invalid name, required format \"{scheme}:{id}\": %s", name)
-	}
-	scheme, id := parts[0], parts[1]
-	switch scheme {
-	case "fidelity":
-		return fidelity.Get(ctx, id)
-	case "ammufg":
-		return ammufg.Get(ctx, ammufg.CodeTypeFund, id)
-	default:
-		return nil, fmt.Errorf("unknown scheme: %s", scheme)
-	}
-}
-
-func prepareTable(ctx context.Context, db *sql.DB) error {
-	_, err := db.Exec(
-		`CREATE TABLE IF NOT EXISTS fund (
-	scheme TEXT,
-	id TEXT,
-	date TEXT,
-	price INTEGER)`)
-	if err != nil {
-		return err
-	}
-	_, err = db.Exec(`CREATE INDEX IF NOT EXISTS fund_scheme_idx ON fund (scheme)`)
-	if err != nil {
-		return err
-	}
-	_, err = db.Exec(`CREATE INDEX IF NOT EXISTS fund_scheme_id_idx ON fund (scheme, id)`)
-	if err != nil {
-		return err
-	}
-	_, err = db.Exec(`CREATE UNIQUE INDEX IF NOT EXISTS fund_scheme_id_date_idx ON fund (scheme, id, date)`)
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func insertFundData(tx *sql.Tx, d FundData) error {
-	_, err := tx.Exec(`INSERT OR REPLACE INTO fund (scheme, id, date, price) VALUES (?, ?, ?, ?)`, d.Scheme, d.ID, d.Date, d.Price)
-	return err
-}
-
-func run(ctx context.Context, dbfile string, items []string) error {
-	if len(items) == 0 {
-		return errors.New("no items")
-	}
-	db, err := sql.Open("sqlite3", dbfile)
-	if err != nil {
-		return err
-	}
-	defer db.Close()
-
-	err = prepareTable(ctx, db)
-	if err != nil {
-		return err
-	}
-
-	tx, err := db.Begin()
-	if err != nil {
-		return err
-	}
-	commit := true
-	defer func() {
-		if !commit {
-			tx.Rollback()
-			return
-		}
-		tx.Commit()
-	}()
-
-	for _, item := range items {
-		d, err := get(ctx, item)
-		if err != nil {
-			commit = false
-			return fmt.Errorf("failed to get %q: %w", item, err)
-		}
-		err = insertFundData(tx, fill(d))
-		if err != nil {
-			commit = false
-			return err
-		}
-	}
-	return nil
-}
-
-func loadItems(name string) ([]string, error) {
-	var out []string
-	f, err := os.Open(name)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	sc := bufio.NewScanner(f)
-	for sc.Scan() {
-		out = append(out, sc.Text())
-	}
-	if err := sc.Err(); err != nil {
-		return nil, err
-	}
-	return out, nil
-}
+var commandSet = subcmd.DefineRootSet(
+	price.Set,
+	fund.Set,
+	database.Set,
+)
 
 func main() {
-	dbfile := flag.String("d", "fund0.db", `database file`)
-	itemfile := flag.String("i", "", `items file`)
-	flag.Parse()
-
-	args := flag.Args()
-	if s := *itemfile; s != "" {
-		items, err := loadItems(s)
-		if err != nil {
-			log.Fatal(err)
-		}
-		if len(items) > 0 {
-			args = append(args, items...)
-		}
-	}
-
-	err := run(context.Background(), *dbfile, args)
+	err := subcmd.Run(commandSet, os.Args[1:]...)
 	if err != nil {
 		log.Fatal(err)
 	}
