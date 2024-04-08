3db633bfe28c1ed77e425f229a1344ad467b3f53
MURAOKA Taro <koron.kaoriya@gmail.com>
2024-04-08 10:44:07 +0900

    test: pictet adapter

---
+++ internal/pictet/pictet_test.go
@@ -0,0 +1,35 @@
+package pictet_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/koron/funddb/internal/pictet"
+)
+
+func TestGet(t *testing.T) {
+	ctx := context.Background()
+
+	for _, name := range []string{
+		"prembrand",
+		"gloin",
+		"quattro",
+	} {
+		d, err := pictet.Get(ctx, name)
+		if err != nil {
+			t.Errorf("failed to get %q: %v", name, err)
+		}
+		if dt := d.Date(); dt.IsZero() {
+			t.Errorf("invalid date %q", name)
+		}
+		if id := d.ID(); id != name {
+			t.Errorf("unmatched ID: want=%s got=%s", name, id)
+		}
+		if p := d.Price(); p <= 0 {
+			t.Errorf("invalid price %q: %d", name, p)
+		}
+		if na := d.NetAssets(); na <= 0 {
+			t.Errorf("invalid net assets %q: %d", name, na)
+		}
+	}
+}
